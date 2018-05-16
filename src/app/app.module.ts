571b67baf372740c7ac258749a8bbc1fa2bc4128
maurozampini <mauro.zampini@hotmail.com>
2018-05-16 00:17:52 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,30 @@
-import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { StatusBar } from '@ionic-native/status-bar';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { LoginPage } from '../pages/login/login';
-import { RegisterPage } from '../pages/register/register';
-import { MenuCreditoPage } from '../pages/menu-credito/menu-credito';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-import { Camera } from '@ionic-native/camera';
+import { StatusBar } from '@ionic-native/status-bar';
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyDCv0vImz6mV8GHALqm-zAKem5n5w0uq9w",
-  authDomain: "appcredito-9672a.firebaseapp.com",
-  databaseURL: "https://appcredito-9672a.firebaseio.com",
-  projectId: "appcredito-9672a",
-  storageBucket: "",
-  messagingSenderId: "870404759120"
-};
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    RegisterPage,
-    LoginPage,
-    MenuCreditoPage
+    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule,
-    AngularFireAuthModule
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    RegisterPage,
-    LoginPage,
-    MenuCreditoPage
+    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    Camera,
-    BarcodeScanner,
-    AngularFireDatabaseModule,
-    AngularFireAuthModule,
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
